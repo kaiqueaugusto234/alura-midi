035f4e68a3cfb26afde103da14a9eb4868ff909b
kaiqueaugusto234 <kaique.dziubate@escola.pr.gov.br>
2025-10-10 07:44:43 -0300

    unkeydown

--- main.js
+++ main.js
@@ -11,30 +11,3 @@ function tocaSom (seletorAudio) {
         console.log('elemento nao encontrado');
     }
 }
-
-const listaDeteclas = document.querySelectorAll('.tecla');
-
-for (let contador = 0; contador < listaDeteclas.length; contador++){
-
-    const tecla = listaDeteclas[contador];
-    const instru = tecla.classList[1];
-    const idAudio = `#som_${instru}`; //template string
-
-    tecla.onclick = function(){
-        tocaSom(idAudio);
-    };
-
-    // add uma classe chamada 'ativa' em todo canto que tem a classe 'tecla'  
-    tecla.onkeydown = function(evento){
-        
-        if (evento.code === 'Space' || evento.code === 'Enter'){
-
-            tecla.classList.add('ativa');
-        }
-    }
-
-    // remove uma classe chamada 'ativa' em todo canto que tem a classe 'tecla'
-    tecla.onkeyup = function(){
-        tecla.classList.remove('ativa');
-    }
-}
